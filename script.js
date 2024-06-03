ece1a9d2037e9e7da0d14fcae0f460a95e7c2559
EssEnemiGz <biscenp@gmail.com>
2024-06-03 11:07:04 -0400

    "Practicas de codigo agregadas"

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function main(){
     if (input.classList.contains('visible')){ // Devuelve un booleano, de si tiene la clase o no
         input.classList.remove('visible'); // Remueve la clase, en caso de tenerla
 
-        const main_container = document.getElementById('sin-empezar');
-        const task = document.createElement('div');
-        task.classList.add('task');
+        const main_container = document.getElementById('sin-empezar'); // document tiene el metodo getElementById, que selecciona el elemento con el id especificado
+        const task = document.createElement('div'); // createElement es usado para crear etiquetas desde JS
+        task.classList.add('task'); // ClassList es una lista como cualquier otra practicamente, por lo que tiene metodos similares, mas no iguales.
 
-        const desc = document.createElement('p');
+        const desc = document.createElement('p'); 
         desc.classList.add('desc');
-        desc.innerText = input.value;
+        desc.innerText = input.value; // Esta logica es simplemente para que el innerText o texto visible de una etiqueta sea igual a lo que hayan escrito en un input
 
-        task.appendChild(desc)
-        main_container.insertBefore(task, input)
+        task.appendChild(desc) // Agregue el p al div 
+        main_container.insertBefore(task, input) // Agrega el div arriba del input, el primer argumento de insertBefore es el elemento a agregar y el segundo el elemento que va a quedar abajo
     } else { // Si es false, agrega la clase
         input.classList.add('visible') 
     }
